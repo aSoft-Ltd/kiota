7e7faf817926075e35635364c982a46327ff139f
andylamax <andylamax@programmer.net>
2025-05-17 13:36:03 +0300

    Fixed manenos

--- file/core/src/commonMain/kotlin/kiota/file/response/utils.kt
+++ file/core/src/commonMain/kotlin/kiota/file/response/utils.kt
@@ -9,17 +9,6 @@ import kiota.MultiPickingResult
 import kiota.PickingExplanation
 import kiota.SinglePickingResult
 
-//fun MultiPickerResponse.toSingle(): SingleFileResponse = when (this) {
-//    is Cancelled -> this
-//    is Denied -> this
-//    is Failure<*> -> this
-//    is Files -> when (val count = files.size) {
-//        0 -> Cancelled
-//        1 -> files.first()
-//        else -> Failure(reasons = listOf(ResponseError.CountLimitExceeded(count, 1)))
-//    }
-//}
-
 fun MultiPickingResult<*>.toSingle(): SinglePickingResult<PickingExplanation> = when (this) {
     is Cancelled -> this
     is Denied -> this
