0b4b2a2b38d51563deb0ae0814b25f905fb9af7b
andylamax <andylamax@programmer.net>
2025-09-28 13:08:38 +0300

    Added manual observer and manual verifier

---
+++ connection/manual/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("multiplatform")
+    kotlin("plugin.serialization")
+    id("tz.co.asoft.library")
+}
+
+description = "A kotlin multiplatform abstraction for choosing files"
+
+kotlin {
+    if (Targeting.JVM) jvm { library() }
+    if (Targeting.JS) js(IR) { library() }
+    if (Targeting.WASM) wasmJs { library() }
+    if (Targeting.WASM) wasmWasi { library() }
+    if (Targeting.OSX) osxTargets() else listOf()
+    if (Targeting.LINUX) linuxTargets() else listOf()
+    if (Targeting.MINGW) mingwTargets() else listOf()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(projects.kiotaConnectionCore)
+        }
+    }
+}
